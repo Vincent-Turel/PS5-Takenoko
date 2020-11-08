8c09614e858f14c4233b6dcd5bdbba22cc7673c9
Vincent-Turel <Vincent-Turel@users.noreply.github.com>
2020-11-08 20:56:48 +0100

    player improved

--- src/main/java/dev/stonks/takenoko/Main.java
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -2,8 +2,7 @@ package dev.stonks.takenoko;
 
 public class Main {
     public static void main(String... args) {
-        RamdomPlayer botRandom = new RamdomPlayer();
-        System.out.println(++botRandom.score);
-
+        RamdomPlayer ramdomPlayer = new RamdomPlayer();
+        System.out.println(++ramdomPlayer.score);
     }
 }
--- src/main/java/dev/stonks/takenoko/Player.java
+++ src/main/java/dev/stonks/takenoko/Player.java
@@ -5,16 +5,42 @@ import java.util.Map;
 import java.util.Random;
 
 public abstract class Player {
-    protected Objectives[] objectives;
+    protected ArrayList<Objectives> objectives;
+    protected int nbObjectivesAchieved;
     protected Random random;
     protected int score;
 
     public Player(){
-        this.objectives = new Objectives[5];
+        this.objectives = new ArrayList<Objectives>();
+        this.nbObjectivesAchieved = 0;
         this.random = new Random();
         this.score = 0;
     }
 
     public abstract Map.Entry<PositionedTile, Tile> putTile (ArrayList<PositionedTile> possiblePosition, ArrayList<Tile> tiles);
 
+    public ArrayList<Objectives> getObjectives() {
+        return this.objectives.clone();
+    }
+
+    public boolean addObjectives(Objectives objectives){
+        if (this.objectives.size() < 5){
+            this.objectives.add(objectives);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void newObjectivesAchieved(Objectives objectives){
+        this.objectives.remove(objectives);
+        this.nbObjectivesAchieved++;
+        this.score += objectives.getObjPt();
+    }
+
+    public void resetPlayer(){
+        this.score = 0;
+        this.nbObjectivesAchieved = 0;
+        this.objectives.clear();
+    }
 }
