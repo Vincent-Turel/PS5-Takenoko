ab7443d5a5be9dad6279794945420f1bfff872a2
Thomas Di Grande <thomas.di-grande@etu.univ-cotedazur.fr>
2020-11-09 11:27:18 +0100

    rajout de la main

---
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -0,0 +1,14 @@
+package dev.stonks.takenoko;
+
+public class Main {
+
+
+    public static String hello() {
+        return "Hello World!";
+    }
+
+    public static void main(String... args) {
+        System.out.println(hello());
+    }
+
+}
