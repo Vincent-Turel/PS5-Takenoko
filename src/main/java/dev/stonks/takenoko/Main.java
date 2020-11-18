917829435cf6e0fa76a40483207e53d515179d55
danlux18 <57712212+danlux18@users.noreply.github.com>
2020-11-18 16:27:39 +0100

    Update Main.java

--- src/main/java/dev/stonks/takenoko/Main.java
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -3,12 +3,17 @@ package dev.stonks.takenoko;
 import java.util.ArrayList;
 
 public class Main {
-    public static final int nbRandomPlayer = 1;
+    public static final int nbRandomPlayer = 2;
     public static final int nbDumbPlayer = 2;
     public static final int nbIntelligentPlayer = 2;
 
     public static void main(String... args) {
         GameManager gameManager = new GameManager(nbRandomPlayer,nbDumbPlayer);
-        gameManager.playNTime(5);
+        try {
+            gameManager.playNTime(5);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
