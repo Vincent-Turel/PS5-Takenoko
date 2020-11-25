35d109ce8f28578bcdfc240222f2262a90e8b9a5
danlux18 <57712212+danlux18@users.noreply.github.com>
2020-11-25 17:10:41 +0100

    Update GameResultsTest.java

--- src/test/java/dev/stonks/takenoko/GameResultsTest.java
+++ src/test/java/dev/stonks/takenoko/GameResultsTest.java
@@ -1,17 +1,31 @@
 package dev.stonks.takenoko;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameResultsTest {
 
+    GameResults res1;
+    GameResults res2;
+
+    @BeforeEach
+    void initialises2games(){
+        res1 = new GameResults(5,4);
+        res2 = new GameResults(6,3);
+    }
+
     @Test
-    void testSommeResults(){
-        GameResults res1 = new GameResults(5,4);
-        GameResults res2 = new GameResults(6,3);
-        assertEquals(res1,new GameResults(5,4));
+    public void testEqualitiesResults(){
+        assertTrue(res1.equals(res1));
         assertFalse(res2.equals(new GameResults(2,2)));
+    }
+
+    @Test
+    public void testResetRes(){
+        res1.reset();
+        assertTrue(res1.getRank()==0);
+        assertTrue(res2.getRank()!=0);
     }
 }
