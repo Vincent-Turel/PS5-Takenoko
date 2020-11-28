0e0efea03007616caa4d7ceb62cc6b378fbcdabd
Léo Pourcelot <leo.pourcelot@etu.unice.fr>
2020-11-28 18:24:23 +0100

    Remove some unnecessary id management system
    Previous implementations of ObjectifMaker relied on the caller to provide a valid
    objective id. This commit instead creates a new, fresh id for every new Objective.

--- src/main/java/dev/stonks/takenoko/ObjectivesMaker.java
+++ src/main/java/dev/stonks/takenoko/ObjectivesMaker.java
@@ -11,13 +11,13 @@ public class ObjectivesMaker {
     /**
      * addAnObjectives it's the factory to make new objective
      *
-     * @param objID n° of objectives
      * @param nbPT nbPT is the number of points give when success
      * @return return objective or null if the new objective already exist, it has the null value.
      *
      * @author the StonksDev team
      */
-    PatternObjective addAnPatternObjectives(int objID, int nbPT, int objType, Pattern pattern) {
+    PatternObjective addAnPatternObjectives(int nbPT, int objType, Pattern pattern) {
+        int objID = nextObjectiveId();
         for (int value : this.listObjectves) {
             if (value == objID) {
                 return null;
@@ -33,34 +33,39 @@ public class ObjectivesMaker {
     public List<Integer> getlistObjectves(){return listObjectves;}
 
     /**
-     * Returns every valid pattern objective available in the game.
+     * Returns the next available objective ID
      */
-    ArrayList<PatternObjective> validPatternObjectives() {
-        int id = listObjectves
+    int nextObjectiveId() {
+        return listObjectves
                 .stream()
                 .max(Comparator.naturalOrder())
                 .map(x -> x + 1)
                 .orElse(0);
+    }
 
+    /**
+     * Returns every valid pattern objective available in the game.
+     */
+    ArrayList<PatternObjective> validPatternObjectives() {
         ArrayList<PatternObjective> ps = new ArrayList();
 
-        ps.add(addAnPatternObjectives(id++, 2, 1, PatternFactory.createTriangle(TileKind.Green)));
-        ps.add(addAnPatternObjectives(id++, 3, 1, PatternFactory.createDiamond(TileKind.Green)));
-        ps.add(addAnPatternObjectives(id++, 5, 1, PatternFactory.createDiamond(TileKind.Yellow, TileKind.Pink)));
-        ps.add(addAnPatternObjectives(id++, 4, 1, PatternFactory.createDiamond(TileKind.Pink, TileKind.Green)));
-        ps.add(addAnPatternObjectives(id++, 3, 1, PatternFactory.createDiamond(TileKind.Yellow, TileKind.Green)));
+        ps.add(addAnPatternObjectives( 2, 1, PatternFactory.createTriangle(TileKind.Green)));
+        ps.add(addAnPatternObjectives( 3, 1, PatternFactory.createDiamond(TileKind.Green)));
+        ps.add(addAnPatternObjectives( 5, 1, PatternFactory.createDiamond(TileKind.Yellow, TileKind.Pink)));
+        ps.add(addAnPatternObjectives( 4, 1, PatternFactory.createDiamond(TileKind.Pink, TileKind.Green)));
+        ps.add(addAnPatternObjectives( 3, 1, PatternFactory.createDiamond(TileKind.Yellow, TileKind.Green)));
 
-        ps.add(addAnPatternObjectives(id++, 3, 1, PatternFactory.createI(TileKind.Yellow)));
-        ps.add(addAnPatternObjectives(id++, 4, 1, PatternFactory.createDiamond(TileKind.Yellow)));
-        ps.add(addAnPatternObjectives(id++, 3, 1, PatternFactory.createC(TileKind.Yellow)));
-        ps.add(addAnPatternObjectives(id++, 2, 1, PatternFactory.createI(TileKind.Green)));
-        ps.add(addAnPatternObjectives(id++, 2, 1, PatternFactory.createC(TileKind.Green)));
+        ps.add(addAnPatternObjectives( 3, 1, PatternFactory.createI(TileKind.Yellow)));
+        ps.add(addAnPatternObjectives( 4, 1, PatternFactory.createDiamond(TileKind.Yellow)));
+        ps.add(addAnPatternObjectives( 3, 1, PatternFactory.createC(TileKind.Yellow)));
+        ps.add(addAnPatternObjectives( 2, 1, PatternFactory.createI(TileKind.Green)));
+        ps.add(addAnPatternObjectives( 2, 1, PatternFactory.createC(TileKind.Green)));
 
-        ps.add(addAnPatternObjectives(id++, 4, 1, PatternFactory.createC(TileKind.Pink)));
-        ps.add(addAnPatternObjectives(id++, 4, 1, PatternFactory.createTriangle(TileKind.Pink)));
-        ps.add(addAnPatternObjectives(id++, 5, 1, PatternFactory.createDiamond(TileKind.Pink)));
-        ps.add(addAnPatternObjectives(id++, 4, 1, PatternFactory.createI(TileKind.Pink)));
-        ps.add(addAnPatternObjectives(id++, 3, 1, PatternFactory.createTriangle(TileKind.Yellow)));
+        ps.add(addAnPatternObjectives(4, 1, PatternFactory.createC(TileKind.Pink)));
+        ps.add(addAnPatternObjectives(4, 1, PatternFactory.createTriangle(TileKind.Pink)));
+        ps.add(addAnPatternObjectives(5, 1, PatternFactory.createDiamond(TileKind.Pink)));
+        ps.add(addAnPatternObjectives(4, 1, PatternFactory.createI(TileKind.Pink)));
+        ps.add(addAnPatternObjectives(3, 1, PatternFactory.createTriangle(TileKind.Yellow)));
 
         return ps;
     }
