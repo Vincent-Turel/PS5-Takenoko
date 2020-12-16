fcf8aaf5a049946c79bef2b03d0bae6099a23d24
Léo Pourcelot <leo.pourcelot@etu.unice.fr>
2020-12-16 10:40:12 +0100

    Add improvment deck
    This class will handle the improvements that are not placed, and allows to bots to know which
    ones are available or not, and to draw them.

---
+++ src/test/java/dev/stonks/takenoko/ImprovementDeckTest.java
@@ -0,0 +1,45 @@
+package dev.stonks.takenoko;
+
+import dev.stonks.takenoko.gameManagement.ImprovementDeck;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ImprovementDeckTest {
+    ImprovementDeck deck;
+
+    @BeforeEach
+    void setup() {
+        deck = new ImprovementDeck();
+    }
+
+    @Test
+    void canDrawWatershedOnlyThreeTimes() {
+        assertDoesNotThrow(() -> deck.drawWatershed().get());
+        assertDoesNotThrow(() -> deck.drawWatershed().get());
+        assertDoesNotThrow(() -> deck.drawWatershed().get());
+
+        assertTrue(deck.drawWatershed().isEmpty());
+    }
+
+    @Test
+    void isAvailableOnlyThreeTimes() {
+        // This test works because canDrawWatershedOnlyThreeTimes worked.
+
+        // Three watershed available.
+        assertTrue(deck.isWatershedAvailable());
+
+        // Two watershed available.
+        deck.drawWatershed();
+        assertTrue(deck.isWatershedAvailable());
+
+        // One watershed available.
+        deck.drawWatershed();
+        assertTrue(deck.isWatershedAvailable());
+
+        // Drawing last watershed.
+        deck.drawWatershed();
+        assertFalse(deck.isWatershedAvailable());
+    }
+}
