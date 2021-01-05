ce78e3afd484d09d206a2b655080cfb3871f8c6d
Vincent-Turel <Vincent-Turel@users.noreply.github.com>
2021-01-05 11:51:37 +0100

    putImprovement

--- src/main/java/dev/stonks/takenoko/Main.java
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -6,7 +6,7 @@ import picocli.CommandLine;
 public class Main {
     public static void main(String... args) {
 
-        new CommandLine(new CommandLineParser()).execute("dumb", "smart", "-n", "150");
+        new CommandLine(new CommandLineParser()).execute("random", "dumb", "smart", "-n", "500");
         //new CommandLine(new CommandLineParser()).execute(args);
     }
 }
