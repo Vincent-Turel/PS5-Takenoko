a923768c1a3e560843225260a41a55936cce8e29
Vincent-Turel <Vincent-Turel@users.noreply.github.com>
2021-01-07 11:12:43 +0100

    fix bug

--- src/main/java/dev/stonks/takenoko/Main.java
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -6,7 +6,7 @@ import picocli.CommandLine;
 public class Main {
     public static void main(String... args) {
 
-        new CommandLine(new CommandLineParser()).execute("random", "dumb", "smart", "-n", "5");
+        new CommandLine(new CommandLineParser()).execute("random", "smart", "-n", "10");
         //new CommandLine(new CommandLineParser()).execute(args);
     }
 }
