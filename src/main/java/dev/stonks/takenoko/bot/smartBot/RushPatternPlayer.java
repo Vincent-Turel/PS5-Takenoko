ddf45b307a382cf3fae22ccd99fb16f6df1605e9
Vincent-Turel <Vincent-Turel@users.noreply.github.com>
2021-01-07 17:02:45 +0100

    j'avance

--- src/main/java/dev/stonks/takenoko/bot/smartBot/RushPatternPlayer.java
+++ src/main/java/dev/stonks/takenoko/bot/smartBot/RushPatternPlayer.java
@@ -1,6 +1,7 @@
 package dev.stonks.takenoko.bot.smartBot;
 
 import dev.stonks.takenoko.bot.Player;
+import dev.stonks.takenoko.gameManagement.Action;
 import dev.stonks.takenoko.objective.ObjectiveKind;
 
 import java.util.ArrayList;
@@ -9,6 +10,15 @@ public class RushPatternPlayer extends SmartPlayer{
 
     public RushPatternPlayer(int id) {
         super(id);
+    }
+
+    @Override
+    public void filterActionToTry(ArrayList<Action> possibleAction) {
+        if (objectives.stream().noneMatch(objective -> objective.getObjType() == ObjectiveKind.PandaObjective))
+            possibleAction.remove(Action.MovePanda);
+
+        if (objectives.stream().noneMatch(objective -> objective.getObjType() == ObjectiveKind.GardenerObjective))
+            possibleAction.remove(Action.MoveGardener);
     }
 
     @Override
