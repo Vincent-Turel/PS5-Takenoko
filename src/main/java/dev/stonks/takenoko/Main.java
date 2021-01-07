bac6ef3167ab5600024bc367bef4a81f5e33cfae
Vincent-Turel <Vincent-Turel@users.noreply.github.com>
2021-01-07 18:03:44 +0100

    fix array

--- src/main/java/dev/stonks/takenoko/Main.java
+++ src/main/java/dev/stonks/takenoko/Main.java
@@ -6,7 +6,9 @@ import picocli.CommandLine;
 public class Main {
     public static void main(String... args) {
 
-        new CommandLine(new CommandLineParser()).execute("random", "smart", "-n", "10"); // <- for test
+        new CommandLine(new CommandLineParser()).execute(
+                "rushPandaPlayer", "rushGardenerPlayer", "equivalentObjectivePlayer", "rushPatternPlayer",
+                "-n", "100"); // <- for test
 
         //finalOutputForDelivery(); // <- for delivery
 
